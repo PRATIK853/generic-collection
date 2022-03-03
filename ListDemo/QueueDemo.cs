e5a5b7ae42bb5574a39c578b204bb81dfa09fc39
Pratik853 <pratikkadam853@gmail.com>
2022-03-03 15:26:41 +0530

    remaining changes

---
+++ ListDemo/QueueDemo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListDemo
+{
+    class QueueDemo
+    {
+
+        static void Main(string[] args)
+        {
+            Queue mydata = new Queue();
+
+            mydata.Enqueue("1");
+            mydata.Enqueue("7");
+            mydata.Enqueue("3");
+            mydata.Enqueue("4");
+            mydata.Enqueue("5");
+            mydata.Enqueue("6");
+
+
+            foreach (var items in mydata)
+            {
+                Console.WriteLine(items);
+            }
+
+            mydata.Dequeue();
+
+            foreach (var items in mydata)
+            {
+                Console.WriteLine(items);
+            }
+
+            Console.ReadKey();
+
+
+
+        }
+        
+
+
+
+    }
+}
