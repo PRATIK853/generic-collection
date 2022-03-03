595bd91cc4faa4a562bcb8da72c8be71bdff7769
Pratik853 <pratikkadam853@gmail.com>
2022-03-03 15:43:49 +0530

    Exception Demo

---
+++ OOProgram/ExceptionDemo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOProgram
+{
+    class ExceptionDemo
+    {
+        static void Main(string[] args)
+        {
+            //int i = 5, y = 0;
+
+            //int result = i / y;  // divideByZeroException
+
+            //Console.WriteLine(result);
+
+
+
+            //int[] arr = { 1, 2, 3, 4 };
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    Console.WriteLine(arr[4]);  //IndexOutOfRangeException 
+            //}
+
+
+            //argumentoutofRange -
+
+            var numbers = new List<int>();
+
+            numbers.Add(2);
+            var index = 1;
+            Console.WriteLine("Removing item at index {0}", index);
+
+            numbers.RemoveAt(index);
+
+            Console.ReadKey();
+
+        }
+    }
+}
